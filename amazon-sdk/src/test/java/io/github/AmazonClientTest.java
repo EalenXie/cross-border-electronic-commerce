1148615008af82d48d1a1e234411c682ae526635
EalenXie <ealenxie@qq.com>
2022-07-19 12:23:22 +0800

    Ebay sdk

--- amazon-sdk/src/test/java/io/github/AmazonClientTest.java
+++ amazon-sdk/src/test/java/io/github/AmazonClientTest.java
@@ -12,7 +12,7 @@ class AmazonClientTest {
         AmazonClient amazonClient= new AmazonClient();
 
         AmazonResponse<AmazonOrder> order = amazonClient.getOrder("114-4947842-2949825",
-                "Atza|IwEBIIYELmf-gSjBZiGTyakvK39D7wGGqerOQBw4Yd6PrSOZX7scT65riemjvDKpTAAtCn0ztFIy3UWs_WFhAypen0uXFg2YXfVdKf7AjEXkG0bCLqw_47wSPjjx6FX3wwPGSvN0zHgNEP8_ZM_w90yI9Tk_HhCbbiLEgC-qpVeCLEiSZQR71OMKiJ7OFM-BIP5h1LWgo_s_UypjCV-yHtWS_teol2UIOgn3cPyfUNXKDLU6T9v1R9lkHzy4_7AXahs00ns0eKeNsYGwEJpDJlu-EtRRDtEmaB15FddE__ZW_S952p184dMR3PtHDLxEUQsOtZ0");
+                "Atza|IwEBIIYELmf-gSjBZiGTyakvK39D7wGGqer2YXfVdKf7AjEXkGs_UypjCV-yHtWS_teol2UIOgn3cPyfUNXKDLU6T9v1R9lkHzy4_7AXahs00ns0eKeNsYGwEJpDJlu-EtRRDtEmaB15FddE__ZW_S952p184dMR3PtHDLxEUQsOtZ0");
 
         System.out.println(order);
     }
